ee579c03cc27e1b62ea3b0508073eb4c4e3c3ed4
shinwoo0715 <55381899+shinwoo0715@users.noreply.github.com>
2025-06-25 20:20:41 +0900

    Add simple guess number game accessible on phones

--- study/urls.py
+++ study/urls.py
@@ -1,20 +1,19 @@
 from django.contrib import admin
-from django.urls import path , re_path , include
+from django.urls import path, re_path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.shortcuts import redirect
 from . import views
 
 urlpatterns = [
-
-    re_path("^$", views.index, name='index'),
-    path("layout/" , views.layout , name='layout'),
-    path("accounts/", include('accounts.urls')),
-    path("accounts/", include("allauth.urls")),
-
-    path("post/" , include("post.urls" , namespace="post")),
-    path("" , lambda r: redirect("post:post_list") , name='root'), 
-
+    re_path(r'^$', views.index, name='index'),
+    path('layout/', views.layout, name='layout'),
+    path('accounts/', include('accounts.urls')),
+    path('accounts/', include('allauth.urls')),
+    path('post/', include('post.urls', namespace='post')),
+    path('game/', views.game, name='game'),
+    path('', lambda r: redirect('post:post_list'), name='root'),
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL ,document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- study/views.py
+++ study/views.py
@@ -1,22 +1,26 @@
-from django.shortcuts import render , redirect , get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404
+
+from post.models import Post
 
 
 def index(request):
     print("index.html 진입")
-
-    return render(request , 'index.html')
+    return render(request, 'index.html')
 
 
 def layout(request):
-  return render(request  ,'layout.html')
+    return render(request, 'layout.html')
 
 
 def post_like(request, pk):
-  post = get_object_or_404(Post, pk=pk)
-  # 중간자 모델 Like 를 사용하여, 현재 post와 request.user에 해당하는 Like 인스턴스를 가져온다.
-  post_like, post_like_created = post.like_set.get_or_create(user=request.user)
+    post = get_object_or_404(Post, pk=pk)
+    post_like, post_like_created = post.like_set.get_or_create(user=request.user)
 
-  if not post_like_created:
-    post_like.delete()
+    if not post_like_created:
+        post_like.delete()
 
     return redirect('post:post_list')
+
+
+def game(request):
+    return render(request, 'game/guess.html')
